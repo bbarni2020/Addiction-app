371893e5e627f4ed19ef1eecba03cfa4cdd3f5a4
Balogh Barnabás <75606424+bbarni2020@users.noreply.github.com>
2025-01-19 20:02:19 +0100

    Update 2025 jan

--- Addiction Free/Addiction Free/Onboarding/AddActivity.swift
+++ Addiction Free/Addiction Free/Onboarding/AddActivity.swift
@@ -17,48 +17,10 @@ struct AddActivity: View {
             ("Smoking", "🚬")
         ]
         
-        var body: some View {
-            VStack() {
-                Text("Choose Your Addiction")
-                    .font(.title)
-                    .fontWeight(.bold)
-                    .padding(.top, 40)
+        var body: some View {}
                 
-                Spacer()
-                ForEach(addictions, id: \.0) { addiction, emoji in
-                    Button(action: {
-                        let fetchDescriptor = FetchDescriptor<Activity>()
-                        
-                        let activities = try? modelContext.fetch(fetchDescriptor)
-                        let newActivity: Activity
-
-                        if let activity = activities?.first {
-                            newActivity = activity
-                        } else {
-                            newActivity = Activity(name: "\(emoji) \(addiction)", hexColor: "FFC0CB")
-                            modelContext.insert(newActivity)
-                        }
-
-                        try? modelContext.save()
-                        selected = true
-                    }) {
-                        HStack {
-                            Text(emoji)
-                                .font(.largeTitle)
-                            Text(addiction)
-                                .font(.title2)
-                                .fontWeight(.semibold)
-                                .padding(.leading, 10)
-                        }
-                        .frame(maxWidth: .infinity, minHeight: 60)
-                        .foregroundColor(Color.secondary)
-                        .background(Color.primary)
-                        .cornerRadius(15)
-                        .padding(.horizontal)
-                    }
-                }
-            }
-        }
+            
+        
     }
 
 #Preview {
